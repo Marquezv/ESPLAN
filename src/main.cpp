3571c882d27a17aff5d5c9c44aa343f9d6db7f55
Marquezv <vmarquezv@outlook.com.br>
2024-05-11 10:58:30 -0300

    Mensagens Broadcast via WebSocket -> MQTT -> WebSocket

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 #include <PubSubClient.h>
 #include <WebSocketsServer.h>
 
+#include <Preferences.h>
+
+#include <ArduinoJson.h>
+
 #ifndef APSSID
 #define APSSID "CardinalWaveAP"
 #define APPSK "password"
@@ -30,7 +34,11 @@ const int MQTT_PORT = 1883;
 const char *MQTT_CLIENT_ID = "ESP8266_001";
 const char *MQTT_USER = "ESP8266_01";
 const char *MQTT_PASSWORD = "";
-const char *TOPIC = "esp8266_01/logs";
+const char *TOPIC = "esp8266_01";
+const char *TOPIC_SERVER = "server";
+// static const char *topic[] = {send, receive}; 
+
+#define BEGIN_OF_PAD 0
 
 WiFiClient client;
 PubSubClient mqttClient(client);
@@ -59,7 +67,44 @@ unsigned long lastConnectTry = 0;
 /** Current WLAN status */
 unsigned int status = WL_IDLE_STATUS;
 
+// Preferences ( ESP - MemoryManagent)
+Preferences preferences;
 
+
+char* formatTopic(const char* sessionId, const char* action, const char *device){
+  // Tamanho total necessário para a string retornada
+  size_t totalLength = strlen(device) + strlen(sessionId) + strlen(action) + 5; // 2 para o caractere de separação e o caractere nulo
+  // Alocando memória para o array de caracteres e copiando os valores
+  char* format = new char[totalLength];
+  snprintf(format, totalLength, "/%s/%s/%s", device, sessionId, action);
+  return format;
+}
+
+// JSON
+const char **deserializationJson(const char* payload){
+  // Allocate JsonBuffer
+  // Use arduinojson.org/assistant to compute the capacity.
+  StaticJsonDocument<200> doc;
+
+  // Deserialize o JSON
+  DeserializationError error = deserializeJson(doc, payload);
+
+    // Verifica se houve erro no processo de deserialização
+  if (error) {
+      Serial.print("Falha ao deserializar JSON: ");
+      Serial.println(error.c_str());
+      return {};
+  }
+  const char* sessionId = doc["session_Id"]; 
+  const char* action = doc["action"];
+
+  char* send = formatTopic(sessionId, action, TOPIC);
+  char* receive = formatTopic(sessionId, action, TOPIC_SERVER);
+
+  static const char *topic[] = {send, receive}; 
+  
+  return topic;
+}
 /** Is this an IP? */
 boolean isIp(String str) {
   for (size_t i = 0; i < str.length(); i++) {
@@ -77,15 +122,42 @@ String toStringIp(IPAddress ip) {
   return res;
 }
 
+char* uint8_to_string(uint8_t valor_uint8) {
+    char* str = (char*)malloc(2 * sizeof(char)); // Aloca memória para a string
+    if (str == NULL) {
+        // Tratamento de erro se a alocação de memória falhar
+        fprintf(stderr, "Erro ao alocar memória\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Convertendo uint8_t para string
+    snprintf(str, 2, "%c", valor_uint8); // Usa snprintf para garantir que não haja estouro de buffer
+
+    return str;
+}
+
+// Save on EEMPROM
+
+
 // MQTT
 void callback(char* topic, byte* payload, unsigned int length)
-{
-    payload[length] = '\0';
-    int value = String((char*) payload).toInt();
+{   
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)payload[i]);
+    }
+    Serial.println();
+    webSocket.broadcastTXT(payload);
+}
 
-    Serial.println(topic);
-    Serial.println(value);
-   
+void messageOverMqtt(const char *topic, const char *payload) {
+    mqttClient.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD );
+    mqttClient.publish(topic, payload);
+    Serial.printf("Send message on %s", topic);
+    Serial.println();
+    // mqttClient.disconnect();
 }
 
 // WebSocket
@@ -102,15 +174,22 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length)
       break;
     case WStype_TEXT:
       Serial.printf("[%u] Received text: %s\n", num, payload);
+      const char **topics = deserializationJson((char*)payload);
       // Send a response back to the client
+      Serial.printf("[%u] Connected from %d.%d.%d.%d\n", num);
       String echoMessage = "Received:  " + String((char*)payload);
       webSocket.sendTXT(num, echoMessage);
+      messageOverMqtt(topics[0], (char*)payload);
+      mqttClient.subscribe(topics[1]);
+      Serial.println(topics[0]);
+      Serial.println(topics[1]);
       break;
   }
 }
 
 /** Redirect to captive portal if we got a request for another domain. Return true in that case so the page handler do not try to handle the request again. */
 boolean captivePortal() {
+  Serial.println(server.hostHeader());
   if (!isIp(server.hostHeader()) && server.hostHeader() != (String(myHostname) + ".net")) {
     Serial.println("Request redirected to captive portal");
     server.sendHeader("Location", String("http://") + (String(myHostname) + ".net"), true);
@@ -131,16 +210,24 @@ void handleRoot() {
   String payloadReturn;
   http.begin(client, "http://cardinalwave.net");
   int httpCode = http.GET();
+
+  String payload = http.getString();
+  int resultat = (strlen(payload.c_str()));
+  Serial.println(resultat);  
+  
+  String payloadSave;
   Serial.print(httpCode);
   if(httpCode > 0 || httpCode == -1) {
     String payload = http.getString();
     Serial.println(payload);
     payloadReturn = payload;
-    server.send(200, "text/html", payloadReturn);
-  }  
-  http.end();
-  payloadReturn = "<h1>Not Found</h1>";
-  server.send(200, "text/html", payloadReturn);
+    String payloadSave = preferences.getString("payload", payloadReturn);
+    server.send(200, "text/html", payloadSave);
+  }
+  server.send(200, "text/html", preferences.getString("payload", payloadReturn));
+  // http.end();
+  // payloadReturn = "<h1>Not Found</h1>";
+  // server.send(200, "text/html", payloadReturn);
 }
 
 void handleNotFound() {
@@ -186,6 +273,11 @@ void setup() {
   server.on("/", handleRoot);
   server.onNotFound(handleNotFound); 
   server.begin();  // Web server start
+
+  preferences.begin("cw-page", false); 
+  preferences.clear();
+
+  // JSON
 
   Serial.println("HTTP server started");
   // loadCredentials();           // Load WLAN credentials from network
@@ -238,13 +330,13 @@ void loop() {
         while (!client.connected()) {
           if (mqttClient.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD )) {
               Serial.println("Connected to MQTT broker");
+              mqttClient.setCallback(callback);
           } else {
               delay(500);
               Serial.print(".");
           }
         }
 
-        mqttClient.subscribe(TOPIC);
         // Setup MDNS responder
         if (!MDNS.begin(myHostname)) {
           Serial.println("Error setting up MDNS responder!");
@@ -257,7 +349,7 @@ void loop() {
         WiFi.disconnect();
       }
     }
-    if (s == WL_CONNECTED) { MDNS.update();  mqttClient.setCallback(callback);}
+    if (s == WL_CONNECTED) { MDNS.update();  }
   }
   // DNS
   dnsServer.processNextRequest();
@@ -266,5 +358,8 @@ void loop() {
 
   // WebSocket
   webSocket.loop();
-  webSocket.broadcastTXT("{mensagem: teste}");
+
+  // MQTT
+  mqttClient.loop();
+  
 }
